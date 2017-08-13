8336ee3a74e3bc4f45bd84b874db8017f21fa817
Kevin O'Donnell <kevin.odonnell@shopify.com>
2017-08-13 10:27:20 -0400

    User.email column added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170805004652) do
+ActiveRecord::Schema.define(version: 20170813142317) do
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170805004652) do
     t.string   "profile_image"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "email"
   end
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+
+  # test "User.new" do
+  #   user = User.new
+  #   user.save
+  #   puts "user.id: #{user.id}"
+  #
+  #   user2 = User.new
+  #   puts "user2.id: #{user2.id}"
+  #   user2.save
+  #   puts "user2.id: #{user2.id}"
   # end
+
+  test "Users has email" do
+    user = User.new(email: 'test@example.com')
+    user.save!
+
+    user2 = User.find(user.id)
+    assert 'test@example.com', user2.email
+
+    User.all.each do |u|
+      puts "id: #{u.id} email: #{u.email}"
+    end
+  end
 end
